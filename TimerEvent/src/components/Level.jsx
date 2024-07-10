9459c062fb1b7baeadad9200430b822d905f9590
Baptiste <baptiste.rousselot@etu.univ-lyon1.fr>
2024-07-10 10:57:17 +0200

    start of level function
    add tutel picture and displaying evolution % texte

--- TimerEvent/src/components/Level.jsx
+++ TimerEvent/src/components/Level.jsx
@@ -1,9 +1,32 @@
+import tutelImage from '../assets/tutel.png'
 
 const Level = () => {
+
+    const percent = ['1%','10%', '20%', '30%', '40%','50%','60%', '70%', '80%', '90%', '100%'];
+    const challenge = ['1%','10%', '20%', '30%', '40%','50%','60%', '70%', '80%', '90%', '100%']
+
     return (
-        <div>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci recusandae ea consequatur praesentium, fugiat ullam sit deserunt autem labore, veritatis incidunt! Aliquid deleniti dolorum, vero voluptas saepe corrupti voluptatem aliquam!</p>
+        <>
+        <div className='level'>
+            <span>20%</span>
+            <img className='levelimage' src={tutelImage}></img>
+            <div className='evolutionlevel' >
+                <ul>
+                    {percent.reverse().map((percent, index) => (
+                    <li key={index}>{percent}</li>
+                    ))}
+                </ul>
+                <ul>
+                    {challenge.reverse().map((challenge, index) => (
+                    <li key={index}>{challenge}</li>
+                    ))}
+                </ul>
+            </div>
         </div>
+        <div>
+            <p>douze</p>
+        </div>
+        </>
     );
 };
 

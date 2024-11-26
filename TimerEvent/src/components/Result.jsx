ebf4c6c9efdf9140651a1ab6b87586a57f6afe6d
Baptiste <baptiste.rousselot@etu.univ-lyon1.fr>
2024-11-26 23:17:36 +0100

    Tab and score update

--- TimerEvent/src/components/Result.jsx
+++ TimerEvent/src/components/Result.jsx
@@ -6,6 +6,14 @@ const Result = ({ scores }) => {
             <p>Baptiste: {scores.Baptiste}</p>
             <p>Agathe: {scores.Agathe}</p>
             <p>Tymau: {scores.Tymau}</p>
+            <p>Ludivine: {scores.Ludivine}</p>
+            <p>Simon: {scores.Simon}</p>
+            <p>Maud: {scores.Maud}</p>
+            <p>Camille: {scores.Camille}</p>
+            <p>Appoline: {scores.Appoline}</p>
+            <p>Marion: {scores.Marion}</p>
+            <p>Emma: {scores.Emma}</p>
+            <p>Chloé: {scores.Chloé}</p>
         </div>
     );
 }
--- TimerEvent/src/object/ScoreCalculator.jsx
+++ TimerEvent/src/object/ScoreCalculator.jsx
@@ -3,7 +3,16 @@ class ScoreCalculator {
         this.scores = {
             "Baptiste": 0,
             "Agathe": 0,
-            "Tymau": 0
+            "Tymau": 0,
+            "Ludivine": 0,
+            "Simon": 0,
+            "Maud": 0,
+            "Camille": 0,
+            "Appoline": 0,
+            "Marion": 0,
+            "Emma": 0,
+            "Chloé": 0,
+            "Titouan": 0,
         };
     }
 
@@ -11,6 +20,15 @@ class ScoreCalculator {
         this.scores.Baptiste += personneScores[0];
         this.scores.Agathe += personneScores[1];
         this.scores.Tymau += personneScores[2];
+        this.scores.Ludivine += personneScores[3];
+        this.scores.Simon += personneScores[4];
+        this.scores.Maud += personneScores[5];
+        this.scores.Camille += personneScores[6];
+        this.scores.Appoline += personneScores[7];
+        this.scores.Marion += personneScores[8];
+        this.scores.Emma += personneScores[9];
+        this.scores.Chloé += personneScores[10];
+        this.scores.Titouan += personneScores[11];
     }
 
     getScores() {
